36259b523c83412b09ae419ffeb47676616b6a16
Nikhil Kanade <nikhil.kanade@snapcommerce.com@Nikhils-MacBook-Air.local>
2025-07-25 23:44:53 -0400

    Updated to Ensure CI passes

--- src/components/Blog/BlogTitle.js
+++ src/components/Blog/BlogTitle.js
@@ -1,14 +1,5 @@
 import CurvedLoop from '../../blocks/TextAnimations/CurvedLoop/CurvedLoop';
-import GooeyNav from '../../blocks/Components/GooeyNav/GooeyNav'
 import './BlogTitle.css';
-import { Link } from 'react-router-dom';
-
-
-// update with your own items
-const items = [
-  { label: "Home", href: "../" },
-  { label: "Portfolio", href: "#" },
-];
 
 export default function BlogTitle(){
     return (
